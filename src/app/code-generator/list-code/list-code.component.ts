b159e3ff704995febf08ccce3f2c49b80ce66c98
Kevin Karahali <kevin.karahali@hotmail.com>
2022-06-20 12:09:03 +0200

    list component show the list with the code

--- src/app/code-generator/list-code/list-code.component.ts
+++ src/app/code-generator/list-code/list-code.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {GeneratorService} from "../services/generator.service";
 
 @Component({
   selector: 'app-list-code',
   templateUrl: './list-code.component.html',
   styleUrls: ['./list-code.component.css']
 })
-export class ListCodeComponent implements OnInit {
+export class ListCodeComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private generatorService: GeneratorService) {
   }
 
+  public get codes() {
+    return this.generatorService.codes
+  }
 }
