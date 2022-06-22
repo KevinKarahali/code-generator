3d566a5b2cf35686afc386ab5a2d5f614b9fc2a8
Kevin Karahali <kevin.karahali@hotmail.com>
2022-06-22 08:51:08 +0200

    Counter no allow numbers under 1

--- src/app/code-generator/counter/counter.component.ts
+++ src/app/code-generator/counter/counter.component.ts
@@ -8,14 +8,14 @@ import {Component, EventEmitter, Input, Output} from '@angular/core';
 export class CounterComponent {
 
   @Input()
-  public number: number = 1;
+  public number: number = 2;
 
   @Output()
   public onNewNumber: EventEmitter<number> = new EventEmitter<number>()
 
   public accumulate(value: number): void {
-    if (this.number < 1) {
-      this.number = 1
+    if (this.number < 2) {
+      this.number = 2
     }
     this.number += value
     this.onNewNumber.emit(this.number)
